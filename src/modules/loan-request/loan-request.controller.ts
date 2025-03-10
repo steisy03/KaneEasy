1a217846839eb3271a220bede8cd0d1259189bc4
steisyh <steisycrisostomo@outlook.com>
2025-03-10 16:54:24 -0400

    fix: Adding guards configuration to loan request

--- src/modules/loan-request/loan-request.controller.ts
+++ src/modules/loan-request/loan-request.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { LoanRequestService } from './loan-request.service';
 import { CreateLoanRequestDto } from '../../common/dto/create-loan-request.dto';
+import { JwtAuthGuard } from '../../common/guards/jwt-auth.guard';
 
 @Controller('loan')
 export class LoanRequestController {
@@ -8,6 +9,7 @@ export class LoanRequestController {
     constructor(private LoanRequestService: LoanRequestService) { }
 
     @Post()
+    @UseGuards(JwtAuthGuard)
     async makeLoanRequest(@Body() CreateLoanRequestDto: CreateLoanRequestDto) {
         return this.LoanRequestService.makeLoanRequest(CreateLoanRequestDto);
     }
